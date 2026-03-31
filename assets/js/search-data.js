b0c6db8deae30f35822377a8957484068be851eb
marco-hmc <45929729+marco-hmc@users.noreply.github.com>
2026-03-31 12:36:14 +0000

    Deploying to gh-pages from @ marco-hmc/marco-hmc.github.io@92f1be6b8d254cc9b0625af0475a9547878913ba 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -4,28 +4,28 @@ const ninja = document.querySelector('ninja-keys');
 // add the home and posts menu items
 ninja.data = [{
     id: "nav-about",
-    title: "about",
+    title: "About",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
     },
   },{id: "nav-blog",
-          title: "blog",
+          title: "Blog",
           description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/blog/";
           },
         },{id: "nav-projects",
-          title: "projects",
+          title: "Projects",
           description: "A growing collection of my cool projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "Just something about me.",
+        },{id: "nav-resume",
+          title: "Resume",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
@@ -589,6 +589,13 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("mailto:%6D%61%72%63%6F.%68%6D%63%68%75%6E@%67%6D%61%69%6C.%63%6F%6D", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/marco-hmc", "_blank");
         },
       },{
       id: 'light-theme',
